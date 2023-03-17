f6d3e5734280f3ea471c4fa682a707521fe7aafa
diveshthakre19 <diveshthakre07@gmail.com>
2023-03-18 01:10:26 +0530

    uselocation hook  for change tab effect

--- src/components/Headder.js
+++ src/components/Headder.js
@@ -1,16 +1,19 @@
 import React from 'react'
+import { Link, useLocation } from "react-router-dom"
 
 const Headder = () => {
+    const Location = useLocation()
+    const currentPath = Location.pathname
     return (
         <div className=' bg-slate-400'>
             <div className='container m-auto flex justify-between py-4'>
                 <h1 className='text-xl md:text-3xl'>Weather Wizard</h1>
                 <div className='flex gap-6 md:text-xl'>
-                    <button>Local weather</button>
-                    <button>Search Weather</button>
+                    <button className={`p-2 ${(currentPath === "/") ? "bg-slate-100" : null}`}><Link to="/">Nearby weather</Link></button>
+                    <button className={`p-2 ${(currentPath === "/search") ? "bg-slate-100" : null}`}><Link to="/search">Search Weather</Link></button>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
